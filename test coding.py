414c8d06949e0f3a56620180c2d749e9cf382a1c
Moayé <114142523+uvsq22205038@users.noreply.github.com>
2023-03-21 08:24:19 +0100

    Update test coding.py

--- test coding.py
+++ test coding.py
@@ -1,6 +1,6 @@
 
     #bouton pour choisir l'orientation
-bouton_bas.grid(column =1, row = 1, rowspan=2)
-bouton_haut.grid(column =1, row = 2, rowspan=2)
+bouton_bas.grid(column =1, row = 1)
+bouton_haut.grid(column =1, row = 2)
 bouton_droit.grid(column =2, row = 2)
 bouton_gauche.grid(column =0, row = 2)
