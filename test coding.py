f6c73a1666fd0c372ac0c1500e00e29b23c0e8f0
moaye.komenan <moaye.komenan@ens.uvsq.fr>
2023-03-21 08:03:11 +0100

    avancement TD

--- test coding.py
+++ test coding.py
@@ -1,4 +1,6 @@
-## IMPORT DES LIBRAIRIES
-from tkinter import *
-from random import randint
-from tkinter.font import Font
+
+    #bouton pour choisir l'orientation
+bouton_bas.grid(column =1, row = 1, rowspan=2, sticky="s")
+bouton_haut.grid(column =1, row = 2, rowspan=2, sticky="n")
+bouton_droit.grid(column =2, row = 2)
+bouton_gauche.grid(column =0, row = 2)
