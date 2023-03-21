39fc53b549215a0e480b273599bf4f64a906768a
Prénom NOM <prenom.nom@ens.uvsq.fr>
2023-03-21 12:18:35 +0100

    Tesr

---
+++ test coding.py
@@ -0,0 +1,13 @@
+#Projet : Fourmi de Langton 
+from tkinter import *
+from random import randint
+from tkinter.font import Font
+hauteur =  500
+largeur = 500
+cases = []
+n = 125
+config_courante = []
+x_fourmi = 0
+y_fourmi = 0
+position_tete_fourmi = 0 
+
