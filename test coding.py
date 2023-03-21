430e0416bfb4c76920a26272a45745b57cd3de2c
Prénom NOM <prenom.nom@ens.uvsq.fr>
2023-03-21 12:41:49 +0100

    PROJET 1

---
+++ test coding.py
@@ -0,0 +1,18 @@
+#programme fourmis de langton
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+titre = tk.title(window, text="Fourmis de LANGTON")
+window.mainloop()
+#Cases de la grille
+cases = []
+''''''''''''''''''''''''''''''
+#la fourmi 
+x_fourmi = 0 
+y_fourmi = 0 
+# la tête 
+position_tete_fourmi = 0 
+# pause
+boutton_pause :
+
