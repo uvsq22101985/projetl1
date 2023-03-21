7f3c51b003808694cd1e06e0de0999abe9f88059
Prénom NOM <prenom.nom@ens.uvsq.fr>
2023-03-21 12:29:06 +0100

    TESTé

--- test coding.py
+++ test coding.py
@@ -1,13 +1,12 @@
 #Projet : Fourmi de Langton 
 from tkinter import *
 from random import randint
-from tkinter.font import Font
 hauteur =  500
-largeur = 500
+largeur = 501
 cases = []
 n = 125
 config_courante = []
 x_fourmi = 0
 y_fourmi = 0
-position_tete_fourmi = 0 
+position_tete_fourmi = 1
 
